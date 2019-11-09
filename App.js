092472144a0541089f3a3cc43d3f7820e33311cc
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-09 20:03:37 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text>Texto Abaixo...</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
